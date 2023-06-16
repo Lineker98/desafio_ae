abc31f856af84a973f9c05817fae6cd9dce7fb8e
LinekerAA <lineker.aguiar98@gmail.com>
2023-06-16 16:28:00 +0000

    Add underscore on id keys

--- models/marts/dim_sales_reason.sql
+++ models/marts/dim_sales_reason.sql
@@ -6,17 +6,17 @@ with
 
     , sales_order_header_sales_reason as (
         select 
-            sales_order_reason.salesorderid			
-            , sales_order_reason.salesreasonid
+            sales_order_reason.salesorder_id			
+            , sales_order_reason.salesreason_id
             , sales_reason.name as sales_reason_name
             , sales_reason.reasontype as reason_type
         from {{ ref('stg_sales_order_header_sales_reason') }} as sales_order_reason
-        left join sales_reason on sales_order_reason.salesreasonid = sales_reason.salesreasonid
+        left join sales_reason on sales_order_reason.salesreason_id = sales_reason.salesreason_id
     )
 
     , transformed as (
         select 
-            row_number() over (order by salesorderid) as sales_order_sk
+            row_number() over (order by salesorder_id) as sales_order_sk
             , *
         from sales_order_header_sales_reason
     )
--- models/marts/dim_territory.sql
+++ models/marts/dim_territory.sql
@@ -9,23 +9,23 @@ with
     )
     , address as (
         select
-            address.addressid
+            address.address_id
             , address.addressline1
             , address.addressline2
             , address.city
-            , address.stateprovinceid
+            , address.stateprovince_id
             , state_province.name as state_name
             , state_province.stateprovincecode as state_code
             , state_province.countryregioncode as country_code
             , country_region.name as country_name
         from {{ ref('stg_address') }} as address
-        left join state_province on address.stateprovinceid = state_province.stateprovinceid
+        left join state_province on address.stateprovince_id = state_province.stateprovince_id
         left join country_region on state_province.countryregioncode = country_region.countryregioncode
     )
 
     , transformed as (
         select 
-            row_number() over (order by addressid) as address_sk
+            row_number() over (order by address_id) as address_sk
             , *
         from address
     )
