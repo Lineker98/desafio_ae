78562dc4b310f5a5bd0e4fadbe2cb3eac67f3dcf
LinekerAA <lineker.aguiar98@gmail.com>
2023-07-02 18:57:57 +0000

    Fix alias to businessentity

--- models/staging/stg_store.sql
+++ models/staging/stg_store.sql
@@ -1,7 +1,7 @@
 with
     store as (
         select
-            businessentityid as billtoaddress_id
+            businessentityid as businessentity_id
             , salespersonid as salesperson_id
             , name as store_name
         from {{ source('dbt_lineker98', 'store') }}
