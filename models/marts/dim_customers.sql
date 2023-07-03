d3e9528260db239a21d65868ace58c3af3e00575
LinekerAA <lineker.aguiar98@gmail.com>
2023-07-03 11:55:53 +0000

    Remove id columns

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -10,14 +10,11 @@ with
     , customer as (
         select 
             customer.customer_id
-            , customer.person_id
-            , customer.store_id
             , store.store_name
             , concat(person.title, person.firstname, person.middlename, person.lastname) as name
             , person.firstname as firstname
             , person.middlename as middlename
             , person.lastname as lastname
-            , customer.territory_id
         from {{ ref('stg_customer') }} as customer
         left join person on customer.person_id = person.businessentity_id
         left join store on customer.person_id = store.salesperson_id
